5ab5f36e4bed527ed4bb84c461d5542fb3819c4d
Arun George Zachariah <az2z7@mail.umkc.edu>
2019-12-02 19:12:09 -0600

    Minor changes for the UI

--- contracts/StoreHash.sol
+++ contracts/StoreHash.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.5.3;
 
 contract SimpleStorage {
-    // Declaring a state variable called storedData of type uint (unsigned integer of 256 bits)
     string storedData;
 
     function set(string memory x) public {
